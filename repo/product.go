ada59c02be87c184d83384182cfa7459f00ade2d
huda.uul <syamsul.huda@weup.jp>
2024-05-10 16:36:49 +0700

    feat: add get n update product

--- repo/product.go
+++ repo/product.go
@@ -13,7 +13,10 @@ import (
 )
 
 type ProductRepo interface {
-	CreateProduct(ctx context.Context, data model.CreatedProduct) (model.CreatedProduct, error)
+	GetProductByID(ctx context.Context, id uuid.UUID) (model.Product, error)
+	GetProduct(ctx context.Context, filterQuery string) ([]model.Product, error)
+	CreateProduct(ctx context.Context, data model.Product) (model.Product, error)
+	UpdateProduct(ctx context.Context, data model.Product) error
 	DeleteProduct(ctx context.Context, id uuid.UUID) error
 }
 
@@ -30,12 +33,12 @@ var createProductQuery = `INSERT INTO product
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
     RETURNING "id", "createdAt"`
 
-func (r *productRepo) CreateProduct(ctx context.Context, data model.CreatedProduct) (model.CreatedProduct, error) {
+func (r *productRepo) CreateProduct(ctx context.Context, data model.Product) (model.Product, error) {
 
 	// Generate UUID for the product ID
 	newUUID, err := uuid.NewRandom()
 	if err != nil {
-		return model.CreatedProduct{}, fmt.Errorf("error generating UUID: %v", err)
+		return model.Product{}, fmt.Errorf("error generating UUID: %v", err)
 	}
 	data.ID = newUUID
 	createdAt := time.Now()
@@ -43,10 +46,25 @@ func (r *productRepo) CreateProduct(ctx context.Context, data model.CreatedProdu
 	err = r.db.QueryRowxContext(ctx, createProductQuery,
 		data.ID, data.Name, data.SKU, data.Category, data.ImageURL, data.Notes, data.Stock, data.Price, data.IsAvailable, data.Location, createdAt).Scan(&data.ID, &data.CreatedAt)
 	if err != nil {
-		return model.CreatedProduct{}, fmt.Errorf("error executing query: %v", err)
+		return model.Product{}, fmt.Errorf("error executing query: %v", err)
 	}
 
 	return data, nil
+}
+
+var updateProductQuery = `UPDATE product
+SET "name"=$1, sku=$2, "category"=$3, stock=$4, price=$5, "imageUrl"=$6, notes=$7, "isAvailable"=$8, "location"=$9
+WHERE id=$10;
+`
+
+func (r *productRepo) UpdateProduct(ctx context.Context, data model.Product) error {
+	err := r.db.QueryRowxContext(ctx, updateProductQuery,
+		data.Name, data.SKU, data.Category, data.Stock, data.Price, data.ImageURL, data.Notes, data.IsAvailable, data.Location, data.ID).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r *productRepo) DeleteProduct(ctx context.Context, id uuid.UUID) error {
@@ -66,4 +84,34 @@ func (r *productRepo) DeleteProduct(ctx context.Context, id uuid.UUID) error {
 	}
 
 	return nil
+}
+
+func (r *productRepo) GetProductByID(ctx context.Context, id uuid.UUID) (model.Product, error) {
+	var product model.Product
+	query := `SELECT * FROM product WHERE id = $1`
+	err := r.db.QueryRowxContext(ctx, query, id).StructScan(&product)
+	if err != nil {
+		return product, err
+	}
+
+	return product, nil
+}
+
+func (r *productRepo) GetProduct(ctx context.Context, filterQuery string) ([]model.Product, error) {
+	var products []model.Product
+	query := `SELECT * FROM product WHERE ` + filterQuery
+	rows, err := r.db.QueryxContext(ctx, query)
+	if err != nil {
+		return products, err
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var product model.Product
+		if err := rows.StructScan(&product); err != nil {
+			return products, err
+		}
+		products = append(products, product)
+	}
+	return products, nil
 }
--- service/product.go
+++ service/product.go
@@ -2,17 +2,24 @@ package service
 
 import (
 	"context"
+	"database/sql"
 	"eniqilo-store/model"
 	"eniqilo-store/repo"
+	cerr "eniqilo-store/utils/error"
 	"errors"
+	"fmt"
+	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/google/uuid"
 )
 
 // ProductService handles business logic related to products.
 type ProductService interface {
-	CreateProduct(ctx context.Context, data model.CreatedProduct) (model.CreatedProduct, error)
+	GetProduct(ctx context.Context, param model.GetProductParam) ([]model.Product, error)
+	CreateProduct(ctx context.Context, data model.Product) (model.Product, error)
+	UpdateProduct(ctx context.Context, data model.Product) (model.Product, error)
 	DeleteProduct(ctx context.Context, id uuid.UUID) error
 }
 
@@ -28,16 +35,34 @@ func NewProductService(repo repo.ProductRepo) ProductService {
 }
 
 // CreateProduct handles the creation of a new product.
-func (s *productService) CreateProduct(ctx context.Context, prod model.CreatedProduct) (model.CreatedProduct, error) {
+func (s *productService) CreateProduct(ctx context.Context, prod model.Product) (model.Product, error) {
 	// Validate the product
 	if err := validateCreateProduct(prod); err != nil {
-		return model.CreatedProduct{}, err
+		return model.Product{}, cerr.New(http.StatusBadRequest, err.Error())
 	}
 
 	return s.repo.CreateProduct(ctx, prod)
 }
 
-func validateCreateProduct(prod model.CreatedProduct) error {
+// UpdateProduct handles the creation of a new product.
+func (s *productService) UpdateProduct(ctx context.Context, prod model.Product) (model.Product, error) {
+	// Validate the product
+	if err := validateCreateProduct(prod); err != nil {
+		return model.Product{}, cerr.New(http.StatusBadRequest, err.Error())
+	}
+
+	_, err := s.repo.GetProductByID(ctx, prod.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return model.Product{}, cerr.New(http.StatusNotFound, "Product not found")
+		}
+		return model.Product{}, cerr.New(http.StatusInternalServerError, "Error updating product")
+	}
+
+	return prod, s.repo.UpdateProduct(ctx, prod)
+}
+
+func validateCreateProduct(prod model.Product) error {
 	// Name validation
 	if prod.Name == "" || len(prod.Name) > 30 {
 		return errors.New("name must not be empty and should be between 1 and 30 characters long")
@@ -98,4 +123,56 @@ func validateCreateProduct(prod model.CreatedProduct) error {
 
 func (s *productService) DeleteProduct(ctx context.Context, id uuid.UUID) error {
 	return s.repo.DeleteProduct(ctx, id)
+}
+
+func (s *productService) GetProduct(ctx context.Context, param model.GetProductParam) ([]model.Product, error) {
+	// generate filter query from param
+	// do request
+	return s.repo.GetProduct(ctx, generateSQLFilter(param))
+}
+
+func generateSQLFilter(params model.GetProductParam) string {
+	var conditions []string
+
+	// Add conditions based on the fields provided
+	if params.ID != nil {
+		conditions = append(conditions, fmt.Sprintf(`"id" = '%s'`, *params.ID))
+	}
+
+	// TODO: explore searchable index
+	if params.Name != nil {
+		name := *params.Name
+		// Append wildcard symbols to allow partial matching
+		name = "%" + strings.ToLower(name) + "%"
+		conditions = append(conditions, fmt.Sprintf(`lower("name") LIKE '%s'`, name))
+	}
+	if params.SKU != nil {
+		conditions = append(conditions, fmt.Sprintf(`"sku" = '%s'`, *params.SKU))
+	}
+	if params.IsAvailable != nil {
+		conditions = append(conditions, fmt.Sprintf(`"isAvailable" = %t`, *params.IsAvailable))
+	}
+	if params.Category != nil {
+		conditions = append(conditions, fmt.Sprintf(`"category" = '%s'`, *params.Category))
+	}
+	if params.InStock != nil {
+		conditions = append(conditions, fmt.Sprintf(`"stock" > 0`))
+	}
+
+	// Combine conditions with AND
+	filter := strings.Join(conditions, " AND ")
+
+	// Add additional clauses such as LIMIT and OFFSET
+	if params.Limit != nil {
+		filter += fmt.Sprintf(" LIMIT %d", *params.Limit)
+	} else {
+		filter += " LIMIT 5"
+	}
+	if params.Offset != nil {
+		filter += fmt.Sprintf(" OFFSET %d", *params.Offset)
+	} else {
+		filter += " OFFSET 0"
+	}
+
+	return filter
 }
